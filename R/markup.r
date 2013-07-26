5559ca51375205bc4f651106d677765aeed7136b
David Hajage <dhajage@gmail.com>
2013-07-26 17:34:54 +0200

    ideas for the main function

---
+++ R/markup.r
@@ -0,0 +1,33 @@
+##' Main function
+##'
+##' Idea for the main function
+##'
+##' @param x x
+##' @param include.rownames if TRUE, appears and strong
+##' @param include.colnames if TRUE, appears and strong (no more header argument)
+##' @param rownames rownames
+##' @param colnames colnames
+##' @param italic logical vector or matrix with columns/cells that must be italic
+##' @param strong logical vector or matrix with columns/cells that must be strong
+##' @param caption (and no more caption.level)
+##' @param align or justify (and no more valign)
+##' @param format format
+##' @param digits digits
+##' @param decimal.mark decimal.mark
+##' @param na.print na.print
+##' @param tgroup (appears strong and aligned, no more style, align... options for them)
+##' @param n.tgroup n.tgroup
+##' @param bgroup bgroup
+##' @param n.bgroup n.bgroup
+##' @param lgroup lgroup
+##' @param n.lgroup n.lgroup
+##' @param rgroup rgroup
+##' @param n.rgroup n.rgroup
+##' @param ... ...
+##'
+##' @return a markup object
+##' @author David Hajage \email{dhajage@@gmail.com}
+markup <- function() {
+
+}
+
